65608ffd92d6646370dd826130195e6e11e05aca
snarlynarwhal <pjlegendreiv@gmail.com>
2024-05-28 15:56:01 -0400

    Added support for scroll rects to inventory uis

--- Runtime/Scripts/UIInventory.cs
+++ Runtime/Scripts/UIInventory.cs
@@ -2,6 +2,7 @@ using HHG.Common.Runtime;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 namespace HHG.InventorySystem.Runtime
 {
@@ -11,6 +12,7 @@ namespace HHG.InventorySystem.Runtime
         public IReadOnlyList<UIInventorySlot> Slots => slots;
 
         [SerializeField] private Vector2Int gridSize;
+        [SerializeField] private Vector2Int viewSize;
         [SerializeField] private Vector2Int slotSize;
         [SerializeField] private int padding;
         [SerializeField] private float spacing;
@@ -21,6 +23,8 @@ namespace HHG.InventorySystem.Runtime
 
         private Lazy<CanvasGroup> _canvasGroup = new Lazy<CanvasGroup>();
         private CanvasGroup canvasGroup => _canvasGroup.FromComponent(this);
+        private Lazy<RectTransform> _rectTransform = new Lazy<RectTransform>();
+        private RectTransform rectTransform => _rectTransform.FromComponent(this);
         private List<UIInventorySlot> slots = new List<UIInventorySlot>();
         private Lazy<IRefreshable<IInventory>> _refreshables = new Lazy<IRefreshable<IInventory>>();
         private IRefreshable<IInventory>[] refreshables => _refreshables.FromComponentsInChildren(this);
@@ -29,7 +33,7 @@ namespace HHG.InventorySystem.Runtime
         {
             if (Application.isPlaying)
             {
-                CreateSlots();
+                CreateSlots(true);
             }
         }
 
@@ -112,16 +116,16 @@ namespace HHG.InventorySystem.Runtime
                 Destroy(slot.gameObject);
             }
 
-            LayoutRebuilder.ForceRebuildLayoutImmediate(slotContainer.GetComponent<RectTransform>());
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
         }
 
-        private void CreateSlots()
+        private void CreateSlots(bool force = false)
         {
+            if (!force && (!slotPrefab || !slotContainer)) return;
+
             int count = gridSize.x * gridSize.y;
-            slots = new List<UIInventorySlot>(count);
-
-            if (!slotPrefab || !slotContainer) return;
-
+            slots ??= new List<UIInventorySlot>(count);
+            slots.Capacity = count; // In case already created
             slotContainer.cellSize = slotSize;
             slotContainer.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
             slotContainer.constraintCount = gridSize.x;
@@ -154,6 +158,37 @@ namespace HHG.InventorySystem.Runtime
                     DestroyImmediate(container.GetChild(container.childCount - 1).gameObject);
                 }
             }
+
+            if (this.TryGetComponentInChildren(out ScrollRect scroll))
+            {
+                if (viewSize.x <= 0)
+                {
+                    viewSize.x = gridSize.x;
+                }
+
+                if (viewSize.y <= 0)
+                {
+                    viewSize.y = gridSize.y;
+                }
+
+                RectTransform rect = scroll.GetComponent<RectTransform>();
+
+                Scrollbar hbar = scroll.horizontalScrollbar;
+                Scrollbar vbar = scroll.verticalScrollbar;
+
+                RectTransform hRect = hbar.GetComponent<RectTransform>();
+                RectTransform vRect = vbar.GetComponent<RectTransform>();
+
+                float addx = viewSize.x == gridSize.x ? 0 : hRect.sizeDelta.y;
+                float addy = viewSize.y == gridSize.y ? 0 : vRect.sizeDelta.x;
+
+                rect.sizeDelta = new Vector2(
+                    (viewSize.x * slotSize.x) + ((viewSize.x - 1) * slotSpacing) + (slotPadding * 2) + addx,
+                    (viewSize.y * slotSize.y) + ((viewSize.x - 1) * slotSpacing) + (slotPadding * 2) + addy
+                );
+            }
+
+            LayoutRebuilder.ForceRebuildLayoutImmediate(rectTransform);
         }
     }
 }
